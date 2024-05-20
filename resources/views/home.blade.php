7ea8aa833c1ffb03a1fa2eea59686c3380abd207
NeilMagon <neil.magon97@gmail.com>
2024-05-20 15:40:55 +0200

    feat: add component

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="text-center">Ciao sono la homepage</h1>
+
+    <div class="container">
+        Homepage molisana
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
 
 
+Route::get('/comics', function(){
+    $comics = config('comics');
+    $data = [
+        'comics' => $comics
+    ];
+    return view('comics', $data);
+})->name('comics');
