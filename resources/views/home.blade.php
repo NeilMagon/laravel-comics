b9cfc8e3893b3dd0dc0f1b57644ac46530e7005b
NeilMagon <neil.magon97@gmail.com>
2024-05-20 15:41:22 +0200

    feta: add route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="text-center">Ciao sono la homepage</h1>
-
-    <div class="container">
-        Homepage molisana
-    </div>
+    
 @endsection
