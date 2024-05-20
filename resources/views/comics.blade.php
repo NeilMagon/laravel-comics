6fa5122bc739e3ecf23079e7740d9586c6b9a552
NeilMagon <neil.magon97@gmail.com>
2024-05-20 16:45:55 +0200

    feat: add main

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -4,4 +4,32 @@
     <section class="hero">
         <img src="{{Vite::asset('resources/img/jumbotron.jpg')}}" alt="">
     </section>
+    <section class="ms-bg">
+        <div class="container p-5">
+            <div class="row py-4 mx-4">
+                @foreach ($comics as $comic)
+                    <div class="col-2 pt-5">
+                        <div class="img-container">
+                            <img src="{{ $comic ['thumb']}}" alt="{{ $comic ['title']}}">
+                        </div>
+                        <div class="text text-white pt-3">{{ $comic ['title']}}</div>
+                    </div>
+                @endforeach
+            </div>
+            <div class="d-flex justify-content-center pb-3">
+                <button type="button" class="btn btn-primary">
+                    LOAD MORE
+                </button>
+            </div>
+        </div>
+    </section>
 @endsection
+
+{{-- <div class="row">
+    <div class="col d-flex align-items-center">
+        <div class="image">
+            <img src="{{ $comic ['thumb']}}" alt="{{ $comic ['title']}}">
+        </div>
+        <p class="text-white ps-2" href="#">{{ $comic ['title']}}</p>
+    </div>
+</div> --}}
