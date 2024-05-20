fbd3bb766e02f488c878d22093656ac2ae8193a1
NeilMagon <neil.magon97@gmail.com>
2024-05-20 16:18:37 +0200

    feat: add homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Comics</h1>
+    <section class="text-center container">
+        <h1>Welcome to DC Homepage</h1>
+        <h5 class="pt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure magnam, deleniti optio, voluptas eaque fugiat voluptatibus ipsum laudantium quia eveniet vitae tempore vel? Ducimus aspernatur corporis quas ipsam sunt porro. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, sed quod possimus eveniet non, error autem nobis quam id illum nam atque! Blanditiis alias fugit ducimus tempore libero possimus obcaecati.</h5>
+    </section>
 @endsection
